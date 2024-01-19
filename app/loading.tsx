22e943437fcc288e8ee8b0b91f6aefd769390348
Abdulloh <abdujabborov6172@gmail.com>
2024-01-19 21:21:32 +0500

    a

---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const loading = () => {
+  return (
+    <div className='fixed top-0 left-0 w-full h-screen flex justify-center items-center text-[24px]'>loading...</div>
+  )
+}
+
+export default loading
